14bdd5a8b0b2b2229dd2d7a10f3a450b5ecd13ab
Matt Casperson <mc.casperson@gmail.com>
2021-10-19 14:35:43 -0700

    add javascript for displaying cards
    Co-authored-by: Aaron Ewert <aaronjewert@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+  $("#javascript-card").click(function() {
+    $(".card").hide();
+    $(".javascript-card .card").slideDown();
+  });
+  $("#operators-card").click(function() {
+    $(".card").hide();
+    $(".operators-card .card").slideDown();
+  });
+  $("#variables-card").click(function() {
+    $(".card").hide();
+    $(".variables-card .card").slideDown();
+  });
+  $("#variable-naming-conventions-card").click(function() {
+    $(".card").hide();
+    $(".variable-naming-conventions-card .card").slideDown();
+  });
+  $("#functions-card").click(function() {
+    $(".card").hide();
+    $(".functions-card .card").slideDown();
+  });
+  $("#methods-card").click(function() {
+    $(".card").hide();
+    $(".methods-card .card").slideDown();
+  });
+  $("#arguments-card").click(function() {
+    $(".card").hide();
+    $(".arguments-card .card").slideDown();
+  });
+  $("#parameters-card").click(function() {
+    $(".card").hide();
+    $(".parameters-card .card").slideDown();
+  });
+  $("#return-card").click(function() {
+    $(".card").hide();
+    $(".return-card .card").slideDown();
+  });
+  $("#chaining-methods-card").click(function() {
+    $(".card").hide();
+    $(".chaining-methods-card .card").slideDown();
+  });
+  $("#strings-card").click(function() {
+    $(".card").hide();
+    $(".strings-card .card").slideDown();
+  });
+  $("#booleans-card").click(function() {
+    $(".card").hide();
+    $(".booleans-card .card").slideDown();
+  });
+  $("#undefined-card").click(function() {
+    $(".card").hide();
+    $(".undefined-card .card").slideDown();
+  });
+  $("#nan-card").click(function() {
+    $(".card").hide();
+    $(".nan-card .card").slideDown();
+  });
+  $("#null-card").click(function() {
+    $(".card").hide();
+    $(".null-card .card").slideDown();
+  });
+  $("#alert-card").click(function() {
+    $(".card").hide();
+    $(".alert-card .card").slideDown();
+  });
+  $("#comments-card").click(function() {
+    $(".card").hide();
+    $(".comments-card .card").slideDown();
+  });
+  $("#jquery-card").click(function() {
+    $(".card").hide();
+    $(".jquery-card .card").slideDown();
+  });
+  $("#attributes-card").click(function() {
+    $(".card").hide();
+    $(".attributes-card .card").slideDown();
+  });
+
+});
